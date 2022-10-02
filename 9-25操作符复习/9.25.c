a60df72fff81c439304179829e58b1537bc88ab0
PandanWU <114854105+PandanWU@users.noreply.github.com>
2022-10-02 17:20:35 +0800

    Add files via upload

---
+++ 9-25操作符复习/9.25.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main(void)
+//{
+//  int a=-1;
+//  int b=a<<1;     //编译器大多采用算数移位方式 ，
+//                  //原符号位不变 负数的话先算出其补码，用补码来移位，再求出原码就是结果
+//  printf("b=%d\n",b); 
+//
+// return 0;
+//}
+
+//按位取反 对数字的补码连同符号位在内一起取反  （该过程是互逆的过程）
+
+//int main(void)
+//{
+//  int a=-1;
+//  int b=~a;       
+//                  
+//  printf("b=%d\n",b); //0
+//
+// return 0;
+//}
+
